d053d461ae632c1ac730e0d88b30a26997c219d0
Ayman Osman <aymano.osman@gmail.com>
2015-07-22 14:05:37 +0100

    init

---
+++ main.elm
@@ -0,0 +1,86 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String
+import Debug
+import StartApp
+
+main =
+  StartApp.start {
+    model = init
+    , view = view
+    , update = update
+  }
+  -- let
+  --     actions = (Signal.mailbox Nothing)
+  --     address = Signal.forwardTo actions.address Just
+  --     model =
+  --       Signal.foldp
+  --        (\(Just action) model -> update action model)
+  --        init
+  --        actions.signal
+  -- in
+  -- Signal.map (view address) model
+
+
+type alias Model = {
+  query : String
+  , friends : List Friend
+  , selection : Maybe String
+  }
+
+type alias Friend = {
+  name : String
+  , photo : String
+  }
+
+init : Model
+init = Model "" friends Nothing
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  let qInput =
+        input
+          [on "input" targetValue (Signal.message address << Query)
+          , value model.query
+          ] []
+      results =
+        let filtered =
+          case model.query of
+            "" -> []
+            s -> filterFriends address s model.friends
+        in
+        ul [] filtered
+      selection  =
+        case model.selection of
+          Nothing -> div [] []
+          Just x -> text x
+  in
+  div [] [qInput, results, selection]
+
+viewFriend address f = li [onClick address (Select f.name)] [text f.name, text f.photo]
+filterFriends address s fs =
+  List.map (viewFriend address)
+    <| List.filter (\f -> String.contains (String.toLower s) (String.toLower f.name)) fs
+
+type Action = Query String | Select Id
+
+type alias Id = String
+
+update action model =
+  let x =
+      case action of
+        Query t ->
+          {model | query <- t}
+        Select n ->
+          {model | selection <- Just n}
+  in
+  x |> Debug.watch "State"
+
+
+friends =
+  [f "Ayman" "", f "Jesus" "", f "Dave" "", f "DJ" "", f "Dean" ""]
+
+f : String -> String -> Friend
+f a b = Friend a b
+
